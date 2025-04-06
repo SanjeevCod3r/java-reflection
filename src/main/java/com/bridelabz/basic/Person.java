0b0f10a2a7bdfa3214a954806e0bd31fc39cdd5d
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-04-06 18:26:59 +0530

    [Sanjeev Jaiswal] ADD: Person class with private field 'age' for reflection access

---
+++ src/main/java/com/bridelabz/basic/Person.java
@@ -0,0 +1,13 @@
+package com.bridelabz.basic;
+
+public class Person {
+    private int age;
+
+    public Person() {
+        this.age = 25; // Default age
+    }
+
+    public void showAge() {
+        System.out.println("Age: " + age);
+    }
+}

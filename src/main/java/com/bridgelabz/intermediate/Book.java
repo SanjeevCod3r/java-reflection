b0ebc9a78f8016fa1d106941e5dc14d9a942b4e1
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-04-06 18:28:51 +0530

    [Sanjeev Jaiswal] ADD: Book class annotated with @Author for annotation reflection demo

---
+++ src/main/java/com/bridgelabz/intermediate/Book.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.intermediate;
+
+
+@Author(name = "Sanjeev Jaiswal")
+public class Book {
+    public void printTitle() {
+        System.out.println("Java Reflection Example");
+    }
+}

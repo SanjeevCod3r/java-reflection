f51e8f094cdd26c5133e4a4ac4458d8e4e663570
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-04-06 18:28:52 +0530

    [Sanjeev Jaiswal] ADD: Configuration class with private static field API_KEY for reflection access

---
+++ src/main/java/com/bridgelabz/intermediate/Configuration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.intermediate;
+
+public class Configuration {
+    private static String API_KEY = "DEFAULT_KEY";
+
+    public static void printApiKey() {
+        System.out.println("API_KEY: " + API_KEY);
+    }
+}
